7831d58e180d2d47c47b7752fa7e78afd6b4215e
t-k- <clock126@126.com>
2020-11-14 06:45:51 +0800

    test/test-authd.js added two more options.

--- test/test-authd.js
+++ test/test-authd.js
@@ -5,10 +5,15 @@ const default_host = 'http://localhost:19721'
 program
   .option('--host <host>', `specify the host. Default: ${default_host}`)
   .option('--password <password>', 'specify the password')
+  .option('--wait <n>', 'wait n seconds before verify')
+  .option('--verify-url <url>', 'overwrite the default verify URL')
 program.parse(process.argv)
 
 const host = program.host || default_host
 const password = program.password || 'changeme!'
+const default_verify_url = `${host}/verify/jwt`
+const verify_url = program.verifyUrl || default_verify_url
+const wait_time = program.wait || 0
 
 ;(async function() {
   const cookie = await axios.post(`${host}/login/jwt`,
@@ -27,7 +32,13 @@ const password = program.password || 'changeme!'
 
   if (cookie) {
     console.log('Cookie:', cookie)
-    await axios.post(`${host}/verify/jwt`,
+
+    console.log(`Wait ${wait_time} seconds before verify @ ${verify_url}`)
+    await new Promise((resolve, reject) => {
+      setTimeout(resolve, wait_time * 1000)
+    })
+
+    await axios.post(verify_url,
       { foo: 'foo', bar: 'bar'},
       {
         headers: {
